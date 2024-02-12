00e365536ba403483be2e5e4c4ac3868a14a16ca
Brian Coutts <bjhcoutts@gmail.com>
2024-02-11 21:44:13 -0800

    added status to comments and articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_12_043417) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_12_051749) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_12_043417) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -62,6 +63,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_12_043417) do
     t.integer "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
